7e17b4672a66b1acadf4cdf9d8308f75a7f2ca9c
Manda21 <chaithanya.m21@gmail.com>
2018-10-11 20:22:13 -0400

    modified list

--- html/list.py
+++ html/list.py
@@ -1,2 +1,17 @@
 thislist=["apple","banana","mango"]
 print(thislist)
+
+thislist=["apple","banana","mango"]
+print(thislist[1])
+
+thislist=["apple","banana","mango"]
+thislist[1]= "orange"
+print(thislist)
+
+thislist=["apple","banana","mango"]
+thislist.insert= (1,"orange")
+print(thislist)
+
+thislist=["apple","banana","mango"]
+thislist.append("orange")
+print(thislist)
